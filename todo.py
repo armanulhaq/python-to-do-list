54ac94f728dda257c517d2c6ed4adedafd5babb3
Arman <armanulhaq10@gmail.com>
2024-09-09 11:21:47 +0530

    add edit option

--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 todo = []
 while True:
-    print("Enter add, show or exit")
+    print("Enter add, show, edit or exit")
     user_input = input()
     user_input = user_input.strip()
     match user_input:
@@ -10,8 +10,10 @@ while True:
         case "show":
             for item in todo:
                 print(item)
+        case "edit":
+            index_to_edit = int(input("Enter the index of the to do you want to edit: ")) -1
+            edited_to_do = input("Enter what you want to write in place of that: ")
+            todo[index_to_edit] = edited_to_do
         case "exit":
-            break;
-        case _:
             break;
 print("Bye!")
