d3321df9786dad33222ce9e734497866dab250f9
Arman <armanulhaq10@gmail.com>
2024-09-12 12:01:39 +0530

    add with context to all of the operations and other bug fixes

--- todo.py
+++ todo.py
@@ -25,13 +25,26 @@ while True:
 
         case "edit":
             index_to_edit = int(input("Enter the index of the to do you want to edit: ")) - 1
+            with open('todos.txt', 'r') as file:
+                todo = file.readlines()
             edited_to_do = input("Enter what you want to write in place of that: ")
-            todo[index_to_edit] = edited_to_do
+            todo[index_to_edit] = edited_to_do + '\n'
+
+            # Write the updated todos list back to the file
+            with open('todos.txt', 'w') as file:
+                file.writelines(todo)
 
         case "complete":
             index_to_complete = int(input("Enter the index that you want to complete: "))
-            todo.pop(index_to_complete - 1)
+            with open('todos.txt', 'r') as file:
+                todo = file.readlines()
 
+            todo_to_remove = todo.pop(index_to_complete - 1)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todo)
+
+            print(f"{todo_to_remove.strip('\n')} has been removed.")
         case "exit":
             break;
 print("Bye!")
