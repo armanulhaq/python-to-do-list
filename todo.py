6c3ab16d12751e5d42584fcd08fe8fffa9b21991
Arman <armanulhaq10@gmail.com>
2024-09-12 11:46:32 +0530

    add with context and other bug fixes

--- todo.py
+++ todo.py
@@ -5,29 +5,33 @@ while True:
     match user_input:
         case "add":
             added_input = input("Enter a todo: ") + '\n'
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(added_input)
 
-            file= open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case "show":
-            file = open('todos.txt', 'r')
-            todo = file.readlines()
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todo = file.readlines()
+
             for index, item in enumerate(todo):
+                item = item.strip('\n')
                 row = f"{index + 1}. {item}"
                 print(row)
+
         case "edit":
             index_to_edit = int(input("Enter the index of the to do you want to edit: ")) - 1
             edited_to_do = input("Enter what you want to write in place of that: ")
             todo[index_to_edit] = edited_to_do
+
         case "complete":
             index_to_complete = int(input("Enter the index that you want to complete: "))
             todo.pop(index_to_complete - 1)
+
         case "exit":
             break;
 print("Bye!")
