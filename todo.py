2446772de4ad776c9fa82bd0f44fe12fe1e06a85
Arman <armanulhaq10@gmail.com>
2024-09-13 12:29:55 +0530

    code improvements

--- todo.py
+++ todo.py
@@ -2,49 +2,51 @@ while True:
     user_input = input("Enter add, show, edit, complete or exit: ")
     user_input = user_input.strip()
 
-    match user_input:
-        case "add":
-            added_input = input("Enter a todo: ") + '\n'
+    if "add" in user_input:
+        added_input = user_input[4:]
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.append(added_input)
+        todos.append(added_input)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "show":
-            with open('todos.txt', 'r') as file:
-                todo = file.readlines()
+    elif "show" in user_input:
+        with open('todos.txt', 'r') as file:
+            todo = file.readlines()
 
-            for index, item in enumerate(todo):
-                item = item.strip('\n')
-                row = f"{index + 1}. {item}"
-                print(row)
+        for index, item in enumerate(todo):
+            item = item.strip('\n')
+            row = f"{index + 1}. {item}"
+            print(row)
 
-        case "edit":
-            index_to_edit = int(input("Enter the index of the to do you want to edit: ")) - 1
-            with open('todos.txt', 'r') as file:
-                todo = file.readlines()
-            edited_to_do = input("Enter what you want to write in place of that: ")
-            todo[index_to_edit] = edited_to_do + '\n'
+    elif "edit" in user_input:
+        index_to_edit = int(user_input[5:]) - 1
+        with open('todos.txt', 'r') as file:
+            todo = file.readlines()
+        edited_to_do = input("Enter what you want to write in place of that: ")
+        todo[index_to_edit] = edited_to_do + '\n'
 
-            # Write the updated todos list back to the file
-            with open('todos.txt', 'w') as file:
-                file.writelines(todo)
+        # Write the updated todos list back to the file
+        with open('todos.txt', 'w') as file:
+            file.writelines(todo)
 
-        case "complete":
-            index_to_complete = int(input("Enter the index that you want to complete: "))
-            with open('todos.txt', 'r') as file:
-                todo = file.readlines()
+    elif "complete" in user_input:
+        index_to_complete = int(user_input[9:])
+        with open('todos.txt', 'r') as file:
+            todo = file.readlines()
 
-            todo_to_remove = todo.pop(index_to_complete - 1)
+        todo_to_remove = todo.pop(index_to_complete - 1)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todo)
+        with open('todos.txt', 'w') as file:
+            file.writelines(todo)
 
-            print(f"{todo_to_remove.strip('\n')} has been removed.")
-        case "exit":
-            break;
+        print(f"{todo_to_remove.strip('\n')} has been removed.")
+    elif "exit" in user_input:
+        break;
+
+    else:
+        print("Invalid response")
 print("Bye!")
