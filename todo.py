66635d52457745dff9ecfc2bd29f0dba766fa7cf
Arman <armanulhaq10@gmail.com>
2024-09-13 16:50:46 +0530

    made the application modular with functions

--- todo.py
+++ todo.py
@@ -1,3 +1,12 @@
+def get_todos(filename):
+    with open('todos.txt', 'r') as file:
+        todos = file.readlines()
+        return todos
+
+def write_todos(filename, todos):
+    with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
 while True:
     user_input = input("Enter add, show, edit, complete or exit: ")
     user_input = user_input.strip()
@@ -5,17 +14,14 @@ while True:
     if user_input.startswith("add"):
         added_input = user_input[4:]
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todo = get_todos('todos.txt')
+        todo.append(added_input + '\n')
 
-        todos.append(added_input + '\n')
-
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos('todos.txt', todo)
 
     elif user_input.startswith('show'):
-        with open('todos.txt', 'r') as file:
-            todo = file.readlines()
+
+        todo = get_todos('todos.txt')
 
         for index, item in enumerate(todo):
             item = item.strip('\n')
@@ -23,27 +29,33 @@ while True:
             print(row)
 
     elif user_input.startswith('edit'):
-        index_to_edit = int(user_input[5:]) - 1
-        with open('todos.txt', 'r') as file:
-            todo = file.readlines()
-        edited_to_do = input("Enter what you want to write in place of that: ")
-        todo[index_to_edit] = edited_to_do + '\n'
+        try:
+            index_to_edit = int(user_input[5:]) - 1
 
-        # Write the updated todos list back to the file
-        with open('todos.txt', 'w') as file:
-            file.writelines(todo)
+            todo = get_todos('todos.txt')
+
+            edited_to_do = input("Enter what you want to write in place of that: ")
+            todo[index_to_edit] = edited_to_do + '\n'
+
+            # Write the updated todos list back to the file
+            write_todos('todos.txt',todo)
+
+        except ValueError:
+            print("Invalid Command")
+            continue
 
     elif user_input.startswith('complete'):
-        index_to_complete = int(user_input[9:])
-        with open('todos.txt', 'r') as file:
-            todo = file.readlines()
+        try:
+            index_to_complete = int(user_input[9:])
+            todo = get_todos('todos.txt')
+            todo_to_remove = todo.pop(index_to_complete - 1)
 
-        todo_to_remove = todo.pop(index_to_complete - 1)
+            write_todos('todos.txt', todo)
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todo)
-
-        print(f"{todo_to_remove.strip('\n')} has been removed.")
+            print(f"{todo_to_remove.strip('\n')} has been removed.")
+        except IndexError:
+            print("Invalid index")
+            continue
 
     elif user_input.startswith('exit'):
         break;
