4f870881c2014103e7bd54e713e0a3308e979343
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-09-17 13:59:48 +0530

    fully implemented web version

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
-def get_todos(filename):
+def get_todos(filename='todos.txt'):
     with open('todos.txt', 'r') as file:
         todos = file.readlines()
         return todos
 
-def write_todos(filename, todos):
+def write_todos(todos, filename='todox.txt'):
     with open('todos.txt', 'w') as file:
             file.writelines(todos)
--- web.py
+++ web.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    #creates a dictionary with "new_todo" as key and the item
+    #you enter as value
+    todos.append(todo)
+    functions.write_todos(todos)
+
 todos = functions.get_todos('todos.txt')
 
 st.title("My To-Do App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder="Add a new todo...")
+st.text_input(label="Add new todo", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
